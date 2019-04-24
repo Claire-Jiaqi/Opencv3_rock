56d6a2c123ac255b07677854c61005fda1620eea
Claire-Jiaqi <jiaqi970602@163.com>
2019-04-23 20:33:06 -0400

    change selectROI

--- main.py
+++ main.py
@@ -8,31 +8,35 @@ def main():
     img = cv2.imread(img_path, 0)
 
     # 创建一个窗口
+    '''
     cv2.namedWindow("image", flags=cv2.WINDOW_AUTOSIZE)
 
     cv2.namedWindow("image_roi", flags=cv2.WINDOW_NORMAL | cv2.WINDOW_FREERATIO)
+'''
+    # use k-means method resize the original photo to 1/4
 
-    cv2.imshow("image", img)
+    img_test2 = cv2.resize(img, (0, 0), fx=0.25, fy=0.25, interpolation=cv2.INTER_NEAREST)
 
     showCrosshair = False
 
-    # 如果为Ture的话 , 则鼠标的其实位置就作为了roi的中心
+    # 如果为True的话 , 则鼠标的其实位置就作为了roi的中心
     # False: 从左上角到右下角选中区域
 
     fromCenter = False
 
     # Select ROI
-    rect = cv2.selectROI("image", img, showCrosshair, fromCenter)
+    rect = cv2.selectROI("image", img_test2, showCrosshair, fromCenter)
 
     print("选中矩形区域")
     (x, y, w, h) = rect
 
     # Crop image
-    imCrop = img[y: y + h, x:x + w]
+    imCrop = img_test2[y: y + h, x: x + w]
 
     # Display cropped image
     cv2.imshow("image_roi", imCrop)
     cv2.imwrite("image_roi.png", imCrop)
+
     cv2.waitKey(0)
 
 
